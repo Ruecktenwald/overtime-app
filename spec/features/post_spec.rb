b1171a16c64942029127f79e4d013215f189da42
Peter Ruecktenwald <ruecktenwald@gmail.com>
2019-03-19 12:21:06 -0400

    refactored index page with post partial-refactored post_spec

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe 'navigate' do
-   
+    before do
+        @user = User.create!(email: "test1@test.com", first_name: "joy", last_name: "wald", password: "secret", password_confirmation: "secret")
+        login_as(@user, :scope => :user)
+    end
+
     feature 'index' do
-      
       before do
-        user = User.create!(email: "test1@test.com", first_name: "joy", last_name: "wald", password: "secret", password_confirmation: "secret")
-        login_as(user, :scope => :user)
-        
         visit posts_path
       end
 
@@ -20,21 +20,19 @@ RSpec.describe Post, type: :model do
       it 'has a title of Posts' do
         expect(page).to have_content(/Posts/)
       end
-      #it 'has a list of posts' do
-       # post1 = Post.create!(date: Date.today, rationale: "Post 1")
-        #post2 = Post.create!(date: Date.today, rationale: "Post 2")
-        #visit posts_path
+      it 'has a list of posts' do
+        post1 = Post.create!(date: Date.today, rationale: "Post 1", user_id: @user.id)
+        post2 = Post.create!(date: Date.today, rationale: "Post 2", user_id: @user.id)
+        visit posts_path
         
-        #expect(page).to have_content 'Post 1'
-        #expect(page).to have_content 'Post 2'
-      #end
+        expect(page).to have_content 'Post 1'
+        expect(page).to have_content 'Post 2'
+      end
     end
 
 
     feature 'creation' do
       before do
-        user = User.create(email: "test@test.com", first_name: "pete", last_name: "wald", password: "secret", password_confirmation: "secret")
-        login_as(user, :scope => :user)
         visit new_post_path
       end
 
@@ -43,7 +41,7 @@ RSpec.describe Post, type: :model do
         expect(page.status_code).to eq(200)
       end
 
-      it 'can be created from new form page and is listed on Posts page' do
+      it 'can be created from new form page' do
         
 
         fill_in 'post[date]', with: Date.today
@@ -51,7 +49,6 @@ RSpec.describe Post, type: :model do
         
         click_on "Save"
 
-        visit posts_path
         expect(page).to have_content("Some Rationale")
 
       end
