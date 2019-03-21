34f60f9dabb0e25ad04e31255069145fa11a9726
Peter Ruecktenwald <ruecktenwald@gmail.com>
2019-03-21 18:47:04 -0400

    added functionality for status

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -51,7 +51,7 @@ end
 private
 
  def post_params
-   params.require(:post).permit(:date,:rationale,:user_id)
+   params.require(:post).permit(:date,:rationale,:user_id,:status)
  end
  
  def set_post
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
 @user = User.create!(email: "test1@test.com", first_name: "joy", last_name: "wald", password: "secret", password_confirmation: "secret")
   
-  puts "1 user created"     
+  puts "1 user created" 
 
+  AdminUser.create!(email: "test@test.com", first_name: "pete", last_name: "wald", password: "secret", password_confirmation: "secret")
+  
+  puts "1 admin created" 
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale", user_id:@user.id)
