af2418174e094c5b9f5e6da04572d5895a885e8d
Peter Ruecktenwald <ruecktenwald@gmail.com>
2019-03-26 09:29:54 -0400

    implemented audit log factory andtested out 3 day default

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,10 @@ end
 
 puts "100 posts have been created"
 
+
+100.times do |audti_log|
+	AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 3.days))
+	
+end
+
+puts "100 audit logs have been created"
--- spec/factories/audit_logs.rb
+++ spec/factories/audit_logs.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :audit_log do
-    user nil
-    status 1
-    start_date "2019-03-25"
-    end_date "2019-03-25"
+    user
+    status 0
+    start_date (Date.today - 3.days)
+    end_date nil
   end
 end
